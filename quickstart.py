11b78defb7f6b15f01e78c42f9958e2482d40347
Xtrah <xtrah@live.com>
2021-02-20 16:49:03 +0100

    Return 1st person's alias

---
+++ quickstart.py
@@ -0,0 +1,53 @@
+# test script, please ignore
+
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/admin.directory.user']
+
+def main():
+    """Shows basic usage of the Admin SDK Directory API.
+    Prints the emails and names of the first 10 users in the domain.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('admin', 'directory_v1', credentials=creds)
+
+    # Call the Admin SDK Directory API
+    print('Admin SDK Directory API script')
+    results = service.users().list(customer='my_customer', maxResults=1, # pylint: disable=maybe-no-member
+                                orderBy='email').execute()
+    users = results.get('users', [])
+
+    if not users:
+        print('No users in the domain.')
+    else:
+        print('Users:')
+        for user in users:
+            # print(u'{0} ({1})'.format(user['primaryEmail'], user['name']['fullName']))
+            print(user['aliases'])
+
+if __name__ == '__main__':
+    main()
